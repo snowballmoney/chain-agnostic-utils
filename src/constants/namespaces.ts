855d91b4819405389f1a9f79461f6e4251372cf6
Alexkiss <alexkiss767@gmail.com>
2024-12-12 16:44:14 +0100

    chore: add more networks

--- src/constants/namespaces.ts
+++ src/constants/namespaces.ts
@@ -1,11 +1,63 @@
 export const NAMESPACES = {
+    LISK: 'lip9',
+    // EVM-based chains
     EVM: 'eip155',
+
+    // Bitcoin-based chains
     BITCOIN: 'bip122',
+
+    // Layer 1 Ecosystems
     COSMOS: 'cosmos',
+    POLKADOT: 'polkadot',
+    KUSAMA: 'kusama',
+    CARDANO: 'cardano',
+    SOLANA: 'solana',
+    RIPPLE: 'xrp',
+    NEAR: 'near',
+    STELLAR: 'stellar',
+    HEDERA: 'hedera',
+    TEZOS: 'tezos',
+    ALGORAND: 'algo',
+    TRON: 'tron',
+
+    // Move-based chains
     MOVEMENT: 'move-mvmt',
     APTOS: 'move-aptos',
     SUI: 'move-sui',
-    SOLANA: 'solana',
+
+    // Other specialized chains
     STARKNET: 'starknet',
-    LISK: 'lip9'
+    FLOW: 'flow',
+    IOTA: 'iota',
+    ERGO: 'ergo',
+    AION: 'aion',
+    ONTOLOGY: 'ontology',
+    NEO: 'neo',
+    VECHAIN: 'vechain',
+    THETA: 'theta',
+    ZILLIQA: 'zil',
+    ELROND: 'egld',
+    WAVES: 'waves',
+    ICON: 'icon',
+    HARMONY: 'harmony',
+    THORCHAIN: 'thor',
+    KLAYTN: 'klaytn',
+    CONFLUX: 'cfx',
+    CELO: 'celo',
+    NERVOS: 'nervos',
+    KADENA: 'kadena',
+    TON: 'ton',
+    TELOS: 'telos',
+    WAX: 'wax',
+    FIRO: 'firo',
+    XDC: 'xdc',
+
+    // Layer 2 and Sidechains
+    ARBITRUM: 'arbitrum',
+    OPTIMISM: 'optimism',
+    METIS: 'metis',
+    ZKSYNC: 'zksync',
+    LINEA: 'linea',
+    BASE: 'base',
+    RONIN: 'ronin'
 } as const;
